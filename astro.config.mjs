c3df82cb8a4ddab2e6d1b69ebb58f63c25cc3e1d
Jorge Morales <luifer991@gmail.com>
2024-09-19 23:01:09 -0500

    build astro dev book by 5.0 version beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind()],
+
+  //  adapter: vercel()
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
+    }
+  },
+
+  adapter: vercel()
+});
